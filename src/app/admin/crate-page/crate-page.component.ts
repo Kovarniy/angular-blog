a1e3527652a77433040fd95c72286ad58a4cda1a
Kovarniy <kovarniy12@yandex.ru>
2021-06-11 19:48:16 +0800

    feat(admin) создал страницы отвечающие за работу администратора

---
+++ src/app/admin/crate-page/crate-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crate-page',
+  templateUrl: './crate-page.component.html',
+  styleUrls: ['./crate-page.component.scss']
+})
+export class CratePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
