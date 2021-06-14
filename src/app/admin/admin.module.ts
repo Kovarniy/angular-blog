04ab153ea55cad28e036fc15c35df685b1c9ed00
Kovarniy <kovarniy12@yandex.ru>
2021-06-14 10:15:13 +0800

    feat(auth.service) добавил сервис аутентификации

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
 import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthService} from "./shared/services/auth.service";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -30,9 +33,11 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
           {path: 'post/:id/edit', component: EditPageComponent}
         ]
       }
-    ])
+    ]),
+    SharedModule
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService]
 })
 export class AdminModule {
 
