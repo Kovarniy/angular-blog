10675c07f20d9e73fb81caa7a325f70a80f94a6d
Kovarniy <kovarniy12@yandex.ru>
2021-06-14 09:54:46 +0800

    feat(interfaces) добавил интерфейс User

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from "../shared/interfaces";
 
 @Component({
   selector: 'app-login-page',
@@ -30,6 +31,11 @@ export class LoginPageComponent implements OnInit {
   submit() {
     if (this.form.invalid) {
       return;
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
     }
   }
 
